2e122a443039831985f83a673ab13c00ab203c8d
nnnn20430 <nnnn20430@mindcraft.si.eu.org>
2015-08-22 00:11:02 +0200

    added a GPL License Copyright notice.

--- nBot.js
+++ nBot.js
@@ -1,4 +1,19 @@
 #!/usr/bin/env node
+// nBot, stupid irc bot made for fun
+// Copyright (C) 2015  nnnn20430 (nnnn20430@mindcraft.si.eu.org)
+//
+// nBot is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// nBot is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /*jshint node: true*/
 /*jshint evil: true*/
